54304aa0d33bb5999d74ee6513bd60a359aa51d7
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-06-27 20:01:43 +0300

    init

---
+++ src/AddProduct.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+export const AddProduct = (props) => {
+  return (
+    <View style={styles.block}>
+      <TextInput style={styles.input} />
+      <Button title="Добавить" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: "row",
+  },
+  input: {
+    width: "70%",
+    borderStyle: "solid",
+    borderBottomWidth: 2,
+    borderBottomColor: "#3949ab",
+  },
+});
